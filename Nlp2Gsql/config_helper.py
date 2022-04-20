597c7099e2b40b2cfa1f52c271fd22ac2fe77287
boopalan jayaraman <boopalan.j@payoda.com>
2022-04-21 05:11:27 +0530

    pointing to the common configuration file for the repository

--- Nlp2Gsql/config_helper.py
+++ Nlp2Gsql/config_helper.py
@@ -1,7 +1,7 @@
 import configparser
 from os import path
 
-config_file_path = path.join(path.dirname(path.abspath(__file__)), 'configuration_dev.ini')
+config_file_path = path.join(path.dirname(path.abspath(__file__)), 'configuration.ini')
 #read configuration file
 config = configparser.ConfigParser()
 config.read(config_file_path)
